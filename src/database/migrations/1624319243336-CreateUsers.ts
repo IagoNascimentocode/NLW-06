0f7f71b9cc3a06e45b81e37985672f3ee6df2ac7
IagoNascimentocode <iago_ns@hotmail.com>
2021-06-21 20:50:40 -0300

    Connection database

---
+++ src/database/migrations/1624319243336-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1624319243336 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
+import "reflect-metadata";
 import express from "express";
+import "./database"
 
 const app = express();
 
-app.get("/test", (request, response) => {
- return response.send("Hello world Get")
-})
 
-app.post("/test-post", (request, response) => {
- return response.send('Hello world method Post')
-})
-
-app.listen(3000, () => console.log("Server is running"))
+app.listen(3000, () => console.log("Server is running"));
